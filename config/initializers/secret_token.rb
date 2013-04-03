7b5d61b7ff68b2559767f506fbb2b880b9dc5347
Julivan Damaceno <julivandamaceno@gmail.com>
2013-04-03 16:12:48 -0300

    Iniciando projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Atrip::Application.config.secret_token = '12c8120fa6914a26ef695ba73896e23ccd7c01ebc3460b3154c22a961caa6e3642a5589c82d275b4577c6ddef8ae29151c1ec9e86a906f01befed86095fd1b4e'
